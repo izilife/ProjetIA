0f7fe770180c1201b1f77170ac2e32521a73f6e6
Mansour <mansour_niang@hotmail.fr>
2015-11-29 19:21:54 +0100

    Début classe Peace

--- src/Package_Peace/Peace.java
+++ src/Package_Peace/Peace.java
@@ -1,9 +1,23 @@
 package Package_Peace;
+import java.util.Scanner;
 
 //je fais un test !
 
 public class Peace implements Joueur{
+	Scanner scan;
+	int role;
+	String name = "Peace";
 
+	public Peace(){
+		scan = new Scanner(Scanner.in);
+	}
+
+	public String getNom(){
+		return this.name;	
+	}
+	public void setRole(int r){
+		this.role = r;
+	}
 	@Override
 	public Domino joue() {
 		// TODO Auto-generated method stub
